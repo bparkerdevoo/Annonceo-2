eb5deb60d11d00854e3baf0302eb057c4bdbcf5a
Dimitri-M <dimitri.malvarez@hotmail.fr>
2017-06-22 14:50:55 +0200

    gestion categorie fini

--- admin/gestion_annonces.php
+++ admin/gestion_annonces.php
@@ -17,7 +17,7 @@ $content = '';
 if(isset($_GET['action']) && $_GET['action'] == 'suppression')
 {
 	$bdd->query("DELETE FROM produit WHERE id_produit= '$_GET[id_produit]'");
-	$content .="<div class ='validation'> Le produit n° ". $_GET['id_produit'] . " a été supprimé </div>";
+	$content .="<div class ='validation'> L'annonce n° ". $_GET['id_annonce'] . " a été supprimé </div>";
 }
 
 /************ AFFICHAGE DES ANNONCES **************************************/
@@ -72,7 +72,20 @@ if(isset($_GET['action']) == 'modification')
 
 		$annonce_actuel = $resultat->fetch(PDO::FETCH_ASSOC); 
 
-		$id_annonce = (isset($annonce_actuel['id_annoce'])) ? $annonce_actuel['id_annonce'] : '';
+		$id_annonce = (isset($annonce_actuel['id_annonce'])) ? $annonce_actuel['id_annonce'] : '';
+		$titre = (isset($annonce_actuel['titre'])) ? $annonce_actuel['titre'] : '';
+		$description_longue = (isset($annonce_actuel['description_longue'])) ? $annonce_actuel['description_longue'] : '';
+		$description_longue = (isset($annonce_actuel['description_longue'])) ? $annonce_actuel['description_longue'] : '';
+		$prix = (isset($annonce_actuel['prix'])) ? $annonce_actuel['prix'] : '';
+		$photo = (isset($annonce_actuel['photo'])) ? $annonce_actuel['photo'] : '';
+		$pays = (isset($annonce_actuel['pays'])) ? $annonce_actuel['pays'] : '';
+		$ville = (isset($annonce_actuel['ville'])) ? $annonce_actuel['ville'] : '';
+		$adresse = (isset($annonce_actuel['adresse'])) ? $annonce_actuel['adresse'] : '';
+		$cp = (isset($annonce_actuel['cp'])) ? $annonce_actuel['cp'] : '';
+		$membre_id = (isset($annonce_actuel['membre_id'])) ? $annonce_actuel['membre_id'] : '';
+		$photo_id = (isset($annonce_actuel['photo_id'])) ? $annonce_actuel['photo_id'] : '';
+		$categorie_id = (isset($annonce_actuel['categorie_id'])) ? $annonce_actuel['categorie_id'] : '';
+		$date_enregistrement = (isset($annonce_actuel['date_enregistrement'])) ? $annonce_actuel['date_enregistrement'] : '';
 	
 
 	$content .= "<h3>Formulaire de modification</h3>";
