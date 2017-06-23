635f305c0d6b86adc6c81c34d69d67284f552129
WilliamB78 <wbutez@outlook.com>
2017-06-23 16:02:51 +0200

    commit accueil + api + haut + app 26/6 16:02

--- accueil.php
+++ accueil.php
@@ -5,14 +5,16 @@ require_once('inc/init.inc.php');
 // requete pour annonce
 
 $annonces = $bdd->query('SELECT
-  annonce.titre, annonce.description_courte, annonce.prix, membre.pseudo, note.note
+  annonce.titre, annonce.description_courte, annonce.prix, annonce.id_annonce, membre.pseudo, note.note
   FROM annonce
   LEFT JOIN membre ON membre.id_membre = annonce.membre_id
   LEFT JOIN note ON annonce.membre_id = note.membre_id1
-  LIMIT 0, 3
+
   ');
+//  LIMIT 0, 3
 
 $results_annonces = $annonces->fetchAll(PDO::FETCH_ASSOC);
+
 
 // requete pour categories
 
@@ -32,53 +34,54 @@ $results_membres = $membres->fetchAll(PDO::FETCH_ASSOC);
 
 // requete pour prix
 
-// affichage en fonction d'un post
-
-if (!empty($_GET)) {
-  debug($_GET);
-}
-
-
 require_once('inc/haut.inc.php');
 
  ?>
 
   <div class="row">
-    <div class="col-sm-12 col-md-8">
-      <!-- affichage des annonces -->
 
-      <?php foreach ($results_annonces as $key => $value): ?>
-      <div class="">
-        <h3><?= $value['titre'] ?></h3>
-        <img src="" alt="">
-        <span><?= $value['description_courte'] ?></span>
-        <span><?= $value['pseudo'] . ' ' . $value['note'] ?></span>
-        <span><?= $value['prix'] ?></span>
+    <!-- affichage des annonces -->
+    <div class="col-sm-12 col-md-4" id="filtres">
+      <form action="./inc/api.php" method="post" id="form_filtres_gauche">
+        <label for="select_categorie">Catégories</label>
+        <select class="form-control" name="categorie" id="select_categorie">
+        <option value="">Toutes les catégories</option>
+        <?php foreach ($results_categories as $key2 => $value2): ?>
+          <option value="<?= $value2['id_categorie'] ?>"><?= $value2['titre'] ?></option>
+        <?php endforeach; ?>
+      </select>
+        <br>
+        <label for="select_membre">Membres</label>
+        <select class="form-control" name="membre" id="select_membre">
+          <option value="">Tous les membres</option>
+        <?php foreach ($results_membres as $key3 => $value3): ?>
+          <option value="<?= $value3['id_membre'] ?>"><?= $value3['pseudo'] ?></option>
+        <?php endforeach; ?>
+      </select><br>
+      </form>
+    </div>
+
+    <!-- affichage du form de gauche -->
+    <div class="col-sm-12 col-md-8" id="annonces">
+
+        <?php foreach ($results_annonces as $key => $value): ?>
+        <div class="panel panel-default" id_annonce="<?= $value['id_annonce'] ?>">
+          <h4 class="text-center"><?= $value['titre'] ?></h3>
+          <div class="panel-body">
+          <img src="" alt="">
+          <span><?= $value['description_courte'] ?></span><br>
+          <span><?= $value['pseudo'] . ' ' . $value['note'] ?></span>
+          <span class=""><?= $value['prix'] ?></span>
+          </div>
+
       </div>
       <?php endforeach; ?>
 
 
     </div>
-    <div class="col-sm-12 col-md-8">
-      <!-- affichage du form de gauche -->
-      <form class="" method="post" id="form_filtres_gauche">
-        <select class="form-control" name="categorie" id="select_categorie">
-        <option value="*">Toutes les catégories</option>
-        <?php foreach ($results_categories as $key2 => $value2): ?>
-          <option value="<?= $value2['id_categorie'] ?>"><?= $value2['titre'] ?></option>
-        <?php endforeach; ?>
-      </select>
-        <select class="form-control" name="membre" id="select_membre">
-          <option value="*">Tous les membres</option>
-        <?php foreach ($results_membres as $key3 => $value3): ?>
-          <option value="<?= $value3['id_membre'] ?>"><?= $value3['pseudo'] ?></option>
-        <?php endforeach; ?>
-      </select>
-      </form>
-    </div>
 
   </div>
 
-<!--*************************** fin de page ************************************** -->
+  <!--*************************** fin de page ************************************** -->
 
-<?php require_once('./inc/bas.inc.php'); ?>
+  <?php require_once('./inc/bas.inc.php'); ?>
